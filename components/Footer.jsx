ca19ce1321fb92273d3514e34c6a9e5217353057
Antilef <fi.antilef@gmail.com>
2024-11-24 23:39:54 -0300

    Add space from footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer ( ) {
     return (
-        <footer className="grow-0 shrink-0 pb-10  text-slate-500">
+        <footer className="grow-0 shrink-0 pb-10 mt-20 text-slate-500">
             <p>Built it in <span>Next.js</span> and <span>Tailwind CSS.</span></p>
         </footer>
     )
